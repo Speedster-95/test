58ad4f8ae81e88eafc9c37a1c9aa0a422cdbe8ce
root <root@ip-172-31-26-186.us-east-2.compute.internal>
2021-02-27 19:23:29 +0000

    first commit

---
+++ arrsort.py
@@ -0,0 +1,4 @@
+import numpy as np
+x = np.array([2, 1, 4, 3, 5])
+y=np.sort(x)
+print(y)
---
+++ mkdir.py
@@ -0,0 +1,5 @@
+import os
+import sys
+import subprocess
+os.chdir('/tmp/')
+subprocess.call(['mkdir', 'fish1 fish2 fish3 fish4 fish5 fish6 fish7 fish8 fish9 fish10'])
